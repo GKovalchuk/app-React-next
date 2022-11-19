aafccc9c1c2bce9083416d22f9cfbcd47d47a178
GKovalchuk <Dintra3@gmail.com>
2022-11-19 13:29:41 +0700

    _document created

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import React from 'react';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
-	return <Component {...pageProps} />;
+	return <>
+		<Head>
+			<title>Next-app</title>
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+		<Component {...pageProps} />
+	</>;
 }
