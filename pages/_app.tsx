f24d4b8d2bc5d246134bbcfcf840d6ba926cf83c
GKovalchuk <Dintra3@gmail.com>
2022-11-22 18:44:42 +0700

    components button and h added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,9 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
 	return <>
 		<Head>
 			<title>Next-app</title>
+			<link rel="preconnect" href="https://fonts.googleapis.com" />
+			<link rel="preconnect" href="https://fonts.gstatic.com" />
+			<link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
 			<link rel="icon" href="/favicon.ico" />
 		</Head>
 		<Component {...pageProps} />
