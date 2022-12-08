93bccc161458a9e93e826a15afb522ef7be9fdd9
GKovalchuk <Dintra3@gmail.com>
2022-12-08 14:07:27 +0300

    Added SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
+import { GetStaticProps } from 'next';
 import React, { useEffect, useState } from 'react';
 import { Button, Htag, P, Rating, Tag } from '../components';
 import { withLayout } from '../layout/Layout';
+import axios from 'axios';
+import { MenuItem } from '../interfaces/menu.interface';
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
 	type numberOrNull = number | null;
 	const [counter, setCounter] = useState<number>(0);
 	const arr: Array<numberOrNull> = [];
@@ -26,8 +29,30 @@ function Home(): JSX.Element {
 			<Tag size='M' color='primary'>СредПрайм</Tag>
 			<Tag size='S' color='green'>МалЗел</Tag>
 			<Rating rating={rating} isEditable={true} setRating={setRating} />
+			<ul>
+				{menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))}
+			</ul>
 		</>
 	);
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+	const firstCategory = 0;
+	const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+		firstCategory
+	});
+
+	return {
+		props: {
+			menu,
+			firstCategory
+		}
+	};
+};
+
+interface HomeProps extends Record<string, unknown> {
+	menu: MenuItem[];
+	firstCategory: number;
+}
