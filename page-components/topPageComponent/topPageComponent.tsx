e8b4f955ba16f554aa093900fd8e5d7368ab320c
GKovalchuk <Dintra3@gmail.com>
2022-12-13 15:53:55 +0300

    Added advantages and skills

--- page-components/topPageComponent/topPageComponent.tsx
+++ page-components/topPageComponent/topPageComponent.tsx
@@ -1,8 +1,8 @@
 import { TopPageComponentProps } from './topPageComponent.props';
 import styles from './topPageComponent.module.css';
-import cn from 'classnames';
 import { HhData, Htag, Tag } from '../../components';
 import { TopLevelCategory } from '../../interfaces/page.interface';
+import { Advantages } from '../../components/Advantages/Advantages';
 
 export const TopPageComponent = ({ page, products, firstCategory }: TopPageComponentProps): JSX.Element => {
 	return (
@@ -15,11 +15,18 @@ export const TopPageComponent = ({ page, products, firstCategory }: TopPageCompo
 			<div>
 				{products && products.map(p => (<div key={p._id}>{p.title}</div>))}
 			</div>
-			<div className={styles.title}>
+			<div className={styles.hhTitle}>
 				<Htag tag='h2'>Вакансии - {page.category}</Htag>
 				{products && <Tag color='red' size='M'>hh.ru</Tag>}
 			</div>
-			{firstCategory == TopLevelCategory.Courses && <HhData {...page.hh} />}
+			{firstCategory == TopLevelCategory.Courses && page.hh && <HhData {...page.hh} />}
+			{page.advantages && page.advantages.length > 0 && <>
+				<Htag tag='h2'>Преимущества</Htag>
+				<Advantages advantages={page.advantages} />
+			</>}
+			{page.seoText && <div className={styles.seo} dangerouslySetInnerHTML={{ __html: page.seoText }} />}
+			<Htag tag='h2'>Получаемые навыки</Htag>
+			{page.tags.map(t => <Tag key={t} color='primary'>{t}</Tag>)}
 		</div>
 	);
 };
