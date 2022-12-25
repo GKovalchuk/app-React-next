aa74757eae293347ddb0337fc0ceca9ebc4ccdb8
GKovalchuk <Dintra3@gmail.com>
2022-12-25 17:54:48 +0300

    Added Product component and Divider

--- page-components/topPageComponent/topPageComponent.tsx
+++ page-components/topPageComponent/topPageComponent.tsx
@@ -1,6 +1,6 @@
 import { TopPageComponentProps } from './topPageComponent.props';
 import styles from './topPageComponent.module.css';
-import { HhData, Htag, Sort, Tag } from '../../components';
+import { HhData, Htag, Product, Sort, Tag } from '../../components';
 import { TopLevelCategory } from '../../interfaces/page.interface';
 import { Advantages } from '../../components/Advantages/Advantages';
 import { SortEnum } from '../../components/Sort/Sort.props';
@@ -20,7 +20,7 @@ export const TopPageComponent = ({ page, products, firstCategory }: TopPageCompo
 				<Sort sort={sort} setSort={setSort} />
 			</div>
 			<div>
-				{sortedProducts && sortedProducts.map(p => (<div key={p._id}>{p.title}</div>))}
+				{sortedProducts && sortedProducts.map(p => (<Product key={p._id} product={p} />))}
 			</div>
 			<div className={styles.hhTitle}>
 				<Htag tag='h2'>Вакансии - {page.category}</Htag>
