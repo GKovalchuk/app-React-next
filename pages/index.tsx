6f4f4c4702286996e7e3581b23b3ddf5b1b3093b
GKovalchuk <Dintra3@gmail.com>
2023-04-26 23:22:58 +0300

    Meta + OpenGraph + YM + Husky added

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,6 @@ import { MenuItem } from '../interfaces/menu.interface';
 import { API } from '../helpers/api';
 
 function Home({ menu }: HomeProps): JSX.Element {
-
 	return (
 		<>
 			<Htag tag='h1'>Заголовок</Htag>
